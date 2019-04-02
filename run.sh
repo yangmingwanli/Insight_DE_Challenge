29f4cb7cc7dcd9e0a65c7818bf1318db3ccc9fd7
yangmingwanli <44122004+yangmingwanli@users.noreply.github.com>
2019-04-01 23:12:05 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 #
 # Use this shell script to compile (if necessary) your code and then execute it. Below is an example of what might be found in this file if your program was written in Python
 #
-#python3 ./src/purchase_analytics.py
+python3 ./src/purchase_analytics.py
